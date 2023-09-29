d84c1d141e2a1da6a7ac9a116ef89440fe15d1db
shurik10 <143745417+shurik10@users.noreply.github.com>
2023-09-29 21:51:30 +0300

    Add files via upload

---
+++ config[1].py
@@ -0,0 +1,14 @@
+adres_servera = "https://2f9ea390-267b-4e1f-98e1-d0208c97ca1b.serverhub.praktikum-services.ru"
+dannyye_zakaza = {
+    "firstName": "Саша",
+    "lastName": "Арчаков",
+    "address": "Москва, Кремль",
+    "metroStation": 4,
+    "phone": "+7 800 555 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2023-09-23",
+    "comment": "Мой первый самокат!",
+    "color": ["BLACK"]
+}
+url_zakazazat = "/api/v1/orders"
+info_treka = "/api/v1/orders/track?t="
